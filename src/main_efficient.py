9a4d996efb824ddf4bce847907b59c70db782a6e
prml_3090 <young221718@gmail.com>
2023-01-20 13:44:29 +0900

    3090 change

--- src/main_efficient.py
+++ src/main_efficient.py
@@ -20,9 +20,9 @@ from solver import Solver
 CFG = {
     'IMG_SIZE':128,
     'MAX_EPOCH':1000,
-    'EARLY_STOP':10,
+    'EARLY_STOP':20,
     'LEARNING_RATE':5e-4,
-    'BATCH_SIZE':16,
+    'BATCH_SIZE':64,
     'SEED':41
 }
 
@@ -40,7 +40,7 @@ seed_everything(CFG['SEED'])
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
 #/////////////////////// SETTING DATA LABEL ////////////////////////////////////// 
-df = pd.read_csv('/home/prml/chanyoung/fire detection/train_info.csv')
+df = pd.read_csv('/home/prml/chanyoung/fire-detection/train_info.csv')
 
 le = preprocessing.LabelEncoder()
 df['class'] = le.fit_transform(df['class'].values)
@@ -48,10 +48,9 @@ df['class'] = le.fit_transform(df['class'].values)
 id2label = {id:label for id, label in enumerate(df['class'].unique())}
 label2id = {label:id for id,label in id2label.items()}
 
-tv_df, test_df, _, _ = train_test_split(df, df['class'].values, test_size=0.1, random_state=CFG['SEED'])
-train_df, val_df, _, _ = train_test_split(tv_df, tv_df['class'].values, test_size=0.25, random_state=CFG['SEED'])
+train_df, val_df, _, _ = train_test_split(df, df['class'].values, test_size=0.25, random_state=CFG['SEED'])
 
-print(f'Trains: [{len(train_df)}], Val: [{len(val_df)}], Test: [{len(test_df)}] ')
+print(f'Trains: [{len(train_df)}], Val: [{len(val_df)}]')
 
 train_dataset = CNN_Dataset(img_paths=train_df['file_path'].values, labels=train_df['class'].values, img_size=CFG['IMG_SIZE'])
 train_loader = DataLoader(train_dataset, batch_size = CFG['BATCH_SIZE'], shuffle=True, num_workers=0)
@@ -69,8 +68,8 @@ for net_name in train_order:
     optimizer = torch.optim.AdamW(model.parameters(), lr=CFG['LEARNING_RATE'])
     criterion = nn.CrossEntropyLoss().to(device)
 
-    model_save_path = f"/home/prml/chanyoung/fire detection/saved_model/efficient_{net_name}.pt"
-    log_save_path = f"/home/prml/chanyoung/fire detection/logs/efficient_{net_name}/"
+    model_save_path = f"/home/prml/chanyoung/fire-detection/saved_model/efficient_{net_name}.pt"
+    log_save_path = f"/home/prml/chanyoung/fire-detection/logs/efficient_{net_name}/"
     
     #/////////////////////// SETTING SOLVER ////////////////////////////////////// 
     solver = Solver(model=model,max_epoch=CFG['MAX_EPOCH'], early_stop=CFG['EARLY_STOP'],
